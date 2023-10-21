919f43a17682cb846046a30472ef69b6d4c0d9db
Han Yujin <133995050+HANYUJINius@users.noreply.github.com>
2023-10-21 20:23:48 +0900

    2022년 파이썬프로그래밍 중간고사 5번

---
+++ Test/Mid22_T4.py
@@ -0,0 +1,13 @@
+#4번
+year = int(input("연도를 입력하세요 : "))
+
+if(year % 4 == 0):
+    if(year % 100 == 0):
+        if(year % 400 == 0):
+            print("윤년입니다.")
+        else:
+            print("윤년이 아닙니다.")
+    else:
+        print("윤년입니다.")
+else:
+    print("윤년이 아닙니다.")
